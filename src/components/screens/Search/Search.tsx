6995f659f5ab8a27cf494ec20d925e330d760dc9
markcoma <dex.go@yandex.ru>
2022-04-09 11:41:01 +0300

    media max-width 480px

--- src/components/screens/Search/Search.tsx
+++ src/components/screens/Search/Search.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Htag } from '../../Htag/Htag'
 import s from './Search.module.css'
 
 export const Search = ():JSX.Element => {
   return (
-    <div className={s.search}>Search</div>
+    <div className={s.search}>
+    <Htag tag={'h1'}>Поиск</Htag>
+    <div className={s.description}>Тестовое задание не предусматривает реализацию контента здесь.</div>
+    </div>
   )
 }
--- src/components/screens/TokenPage/TokenPage.tsx
+++ src/components/screens/TokenPage/TokenPage.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Htag } from '../../Htag/Htag'
 import s from './TokenPage.module.css'
 
-export const TokenPage = ():JSX.Element => {
+export const TokenPage = (): JSX.Element => {
   return (
-    <div className={s.tokenPage}>TokenPage</div>
+    <div className={s.tokenPage}>
+      <Htag tag={'h1'}>Наименование токена</Htag>
+      <div className={s.description}>Тестовое задание не предусматривает реализацию контента здесь.</div>
+    </div>
   )
 }
