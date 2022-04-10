e70b02fb943bb5fd81dd08ffc34ffc92fe7f0585
markcoma <dex.go@yandex.ru>
2022-04-10 16:47:11 +0300

    search adaptive

--- src/components/screens/Search/Search.tsx
+++ src/components/screens/Search/Search.tsx
@@ -29,7 +29,8 @@ export const Search = (): JSX.Element => {
 
   return (
     <div className={s.search}>
-      <Htag tag={'h1'}>Поиск</Htag>
+      <div className={s.title}><Htag tag={'h1'}>Поиск</Htag></div>
+      
       <div className={s.description}>
         <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
           <input
